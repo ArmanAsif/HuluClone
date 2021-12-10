1eba85712c99d15bada98026d73d0863db1148d7
ArmanAsif <arman.cse.ruet@gmail.com>
2021-12-10 03:55:25 -0800

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import {
+	HomeIcon,
+	BadgeCheckIcon,
+	CollectionIcon,
+	LightningBoltIcon,
+	SearchIcon,
+	UserIcon,
+} from "@heroicons/react/outline";
+import Image from "next/image";
+import IconHeader from "./IconHeader";
+
+const Header = () => {
+	return (
+		<header>
+			<div className="flex flex-grow justify-evenly max-w-2xl">
+				<IconHeader title="HOME" Icon={HomeIcon} />
+				<IconHeader title="TRENDING" Icon={LightningBoltIcon} />
+				<IconHeader title="VERIFIED" Icon={BadgeCheckIcon} />
+				<IconHeader title="COLLECTIONS" Icon={CollectionIcon} />
+				<IconHeader title="SEARCH" Icon={SearchIcon} />
+				<IconHeader title="ACCOUNT" Icon={UserIcon} />
+			</div>
+
+			<Image
+				className="object-contain"
+				src="https://links.papareact.com/ua6"
+				width={200}
+				height={100}
+			/>
+		</header>
+	);
+};
+
+export default Header;

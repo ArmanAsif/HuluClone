5c6eb7fd4967edbf2d64494efd6c58701d9f4717
ArmanAsif <arman.cse.ruet@gmail.com>
2021-12-10 15:01:49 -0800

    API Integration Done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import NavMenu from "../components/NavMenu";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
 export default function Home() {
 	return (
@@ -12,8 +14,14 @@ export default function Home() {
 			</Head>
 
 			<Header />
-
 			<NavMenu />
+			<Results />
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+
+	const request = await fetch(`https://api.themoviedb.org/3${requests[genre]}`);
 }
